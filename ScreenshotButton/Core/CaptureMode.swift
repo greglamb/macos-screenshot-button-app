2ef8e7f6de2829c8917e061fb9869f02e386794f
Greg Lamb <email@greglamb.dev>
2026-04-14 17:49:47 -0700

    refactor: drop public access and tautological enum tests

--- ScreenshotButton/Core/CaptureMode.swift
+++ ScreenshotButton/Core/CaptureMode.swift
@@ -1,6 +1,4 @@
-import Foundation
-
-public enum CaptureMode: Equatable, Sendable {
+enum CaptureMode: Equatable, Sendable {
     case window
     case area
 }
--- ScreenshotButton/Core/SinkKind.swift
+++ ScreenshotButton/Core/SinkKind.swift
@@ -1,6 +1,4 @@
-import Foundation
-
-public enum SinkKind: Equatable, Sendable {
+enum SinkKind: Equatable, Sendable {
     case toFile
     case toClipboard
 }
